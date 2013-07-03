f394975e6784d913678796366a367060a1e1b29d
John Diss <john.diss@newgrove.com>
2013-07-03 17:03:35 +0100

    more analysis extension methods

--- src/NHibernateClient/Analysis/CriteriaEx.cs
+++ src/NHibernateClient/Analysis/CriteriaEx.cs
@@ -1,4 +1,5 @@
-﻿using NHibernateClient.Criterion;
+﻿using System.Collections.Generic;
+using NHibernateClient.Criterion;
 using NHibernateClient.Impl;
 
 namespace NHibernateClient.Analysis
@@ -14,5 +15,78 @@ namespace NHibernateClient.Analysis
         {
             return crit.impl;
         }
+
+        public static IEnumerable<ICriterion> GetCriterion(this Junction junction)
+        {
+            return junction.criteria;
+        }
+
+        public static SubQueryDescriptor GetSubQueryDescription(this SubqueryExpression expr)
+        {
+            var a = new SubQueryDescriptor
+                    {
+                        CriteriaImpl = expr.criteriaImpl,
+                        Op = expr.op,
+                        PrefixOp = expr.prefixOp,
+                        Quantifier = expr.quantifier
+                    };
+            return a;
+        }
+
+        public static PropertySubQueryDescriptor GetPropertySubQueryDescription(this PropertySubqueryExpression expr)
+        {
+            SubQueryDescriptor b = expr.GetSubQueryDescription();
+            return new PropertySubQueryDescriptor(b)
+                   {
+                       PropertyName = expr.propertyName
+                   };
+        }
+
+        public static SimpleSubQueryDescriptor GetSimpleSubQueryDescription(this SimpleSubqueryExpression expr)
+        {
+            SubQueryDescriptor b = expr.GetSubQueryDescription();
+            return new SimpleSubQueryDescriptor(b)
+                   {
+                       Value = expr.value
+                   };
+        }
+    }
+
+    public class PropertySubQueryDescriptor : SubQueryDescriptor
+    {
+        public PropertySubQueryDescriptor(SubQueryDescriptor sqdBase)
+            : base(sqdBase) {}
+
+        public string PropertyName { get; set; }
+    }
+
+
+    public class SimpleSubQueryDescriptor : SubQueryDescriptor
+    {
+        public SimpleSubQueryDescriptor(SubQueryDescriptor sqdBase)
+            : base(sqdBase) {}
+
+        public object Value { get; set; }
+    }
+
+    public class SubQueryDescriptor
+    {
+        public SubQueryDescriptor() {}
+
+        public SubQueryDescriptor(SubQueryDescriptor sqdBase)
+        {
+            CriteriaImpl = sqdBase.CriteriaImpl;
+            Op = sqdBase.Op;
+            PrefixOp = sqdBase.PrefixOp;
+            Quantifier = sqdBase.Quantifier;
+        }
+
+        public CriteriaImpl CriteriaImpl { get; set; }
+
+        public string Op { get; set; }
+
+        public bool PrefixOp { get; set; }
+
+        public string Quantifier { get; set; }
     }
 }
