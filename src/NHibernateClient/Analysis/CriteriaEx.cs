1270318a74cacb49687ee3adbdb3ce3c56deebbb
John Diss <john.diss@newgrove.com>
2013-07-03 13:34:19 +0100

    added utility to return CriteriaImpl from DetachedQuery or Query

---
+++ src/NHibernateClient/Analysis/CriteriaEx.cs
@@ -0,0 +1,18 @@
+﻿using NHibernateClient.Criterion;
+using NHibernateClient.Impl;
+
+namespace NHibernateClient.Analysis
+{
+    public static class CriteriaAnalysisEx
+    {
+        public static CriteriaImpl GetCriteriaImpl(this DetachedCriteria crit)
+        {
+            return crit.impl;
+        }
+
+        public static CriteriaImpl GetCriteriaImpl(this QueryOver crit)
+        {
+            return crit.impl;
+        }
+    }
+}
